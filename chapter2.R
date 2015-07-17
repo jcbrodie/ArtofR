844b18e0eac47c06b4ecffc6a5bd8344d491dbdb
jcbrodie <janebrodie@gmail.com>
2015-07-17 23:05:53 +0300

    Added chapter2

---
+++ chapter2.R
@@ -0,0 +1,8 @@
+#####
+# CHAPTER 2: VECTORS
+# The Art of R Programming, by Norman Matloff
+#
+# Focus of this chapter: Recycling, Filtering, Vectorization
+
+#####
+# 2.1: Scalars, Vectors, Arrays, and Matrices
